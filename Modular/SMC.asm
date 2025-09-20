255452e75e50b71c63c7e3ef2c1b4de6c56f572b
Sukasa <nick@greenfirework.com>
2025-09-19 18:45:03 -0700

    Improve SMC chip documentation

--- Modular/SMC.asm
+++ Modular/SMC.asm
@@ -1,9 +1,9 @@
 # SPSM PROJECT - SMC SYSTEM MONITOR CHIP
 # Revision: 2025-09-19
 # Variant: Stack-Driven
-# Timing: ?
+# Timing: Not synchronized
 # Format: n: ReferenceId (53)
-# Monitors all IC10 chips for a fault state, and illuminates a FAULT indicator.  For registered IC10 chips
+# Monitors all IC10 chips for a fault state, and illuminates a FAULT indicator.  For registered IC10 chips, offers the ability to power-cycle any chips that are in error
 # Does not interact with any S-PSM systems or data other than the IC10 error states, power states (for resets), and the FAULT indicator/RESET button
 
   alias FaultIndicator d0
