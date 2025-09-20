7429be2788f64261763879fc16af9c88c3b03700
Sukasa <nick@greenfirework.com>
2025-09-19 18:40:59 -0700

    Added SMC chip for system monitoring

---
+++ Modular/SMC.asm
@@ -0,0 +1,28 @@
+# SPSM PROJECT - SMC SYSTEM MONITOR CHIP
+# Revision: 2025-09-19
+# Variant: Stack-Driven
+# Timing: ?
+# Format: n: ReferenceId (53)
+# Monitors all IC10 chips for a fault state, and illuminates a FAULT indicator.  For registered IC10 chips
+# Does not interact with any S-PSM systems or data other than the IC10 error states, power states (for resets), and the FAULT indicator/RESET button
+
+  alias FaultIndicator d0
+  alias ResetButton d1
+  
+start:
+  yield
+  lb r15 1512322581 Error Sum
+  s d0 On r15
+  bdns d1 start
+  l r0 d1 Setting
+  beqz r0 start
+  move sp 512
+next:
+  pop r0
+  bdns r0 start
+  l r1 r0 Error
+  beqz r0 next
+  s r0 On 0
+  yield
+  s r0 On 1
+  j next
