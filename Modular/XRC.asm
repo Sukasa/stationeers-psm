78425b48e482862f949ba8e7f601ed9ee5fbc055
Sukasa <nick@greenfirework.com>
2025-09-19 22:12:48 -0700

    FEATURE: Adds ability to select PassThrough or fixed value for XRC

--- Modular/XRC.asm
+++ Modular/XRC.asm
@@ -1,25 +1,27 @@
 # SPSM PROJECT - XRC INTERRUPT ROUTER CHIP
 # Revision: 2025-09-19
 # Variant: Stack-Driven
-# Timing: 4/loop + 6/mapping
-# Format: n: ReferenceID (53), n-1: Destination (53)
+# Timing: 4/loop + 9/mapping
+# Format: n: ReferenceID (53), n-1: Write Value (29) | Destination (14)
 # Performs condition transfers from device Setting value to STACK UNIT
 # Only transfers non-zero values, relies on PLC or ASP to clear Zone RAM value
-# XRC can process 20-24 inputs per tick depending on how many are nonzero
+# XRC can process 13-24 inputs per tick depending on how many are nonzero
 
-alias STACK_UNIT d0
-
+  alias STACK_UNIT d0
   define Type Setting
 
 start:
   yield
-  move sp 512
+  move sp 34
   pop r0
   
 loop:
   pop r1
   l r2 r0 Type
   beqz r3 next
+  srl r4 r1 14
+  and r1 r1 8191
+  select r3 r4 r4 r3
   set d0 r1 r3
 next:
   pop r0
