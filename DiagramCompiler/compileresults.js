49a69092a20e73c6a90b41b4455d2b0fe4d0f4ba
Tiogshi Laj <tiogshi@gmail.com>
2025-11-08 21:45:02 -0800

    Initial ugly compile results rendering!

--- DiagramCompiler/compileresults.js
+++ DiagramCompiler/compileresults.js
@@ -3,9 +3,47 @@
 
 function renderCompile(D, S) {
 	const result = S.compilations[S.activeZone];
+	let activeProc = S.compile_proc ?? undefined;
 	console.log(result);
 
 	document.body.classList.add('showing-compile');
 	preRerender(() => document.body.classList.remove('showing-compile'));
-	return [["div", "=TODO compilation result"]];
+	const root = [];
+
+	if( !result?.reports?.length )
+		return [["div", "=No compilation result; hit Compile to run!"]];
+
+	const processors = Object.keys(result.code)
+		.map(k => ({proc:D.FindObject(k), code:result.code[k]}))
+		.map(({proc,code}) => ({proc, code, label: metanode_db[proc.type].Name(proc)}))
+		.sort(({label:a}, {label:b}) => a < b ? -1 : +1);
+
+	if( processors.length > 0 ) {
+		if( !processors.find(({proc}) => proc.id === activeProc) )
+			activeProc = processors[0].proc.id;
+		const active = processors.find(({proc}) => proc.id === activeProc);
+
+		root.push(
+			$("select", {value: activeProc, '?change': evt => {
+				S.compile_proc = evt.currentTarget.value;
+				rerender();
+			}}, processors.map(p => ["option", {value:p.proc.id}, "="+p.label])),
+			
+			$("div", {className: 'code'}, [["pre", "="+active.code]]),
+			$("hr"),
+		);
+	}
+
+	//TODO: filter and sort reports!
+	const filteredReports = result.reports;
+
+	let lastGroup = null;
+	root.push($("div", {className: 'reports'}, filteredReports.flatMap(r => {
+		//TODO: format report lines in detail!
+		
+		// For now, just splat out the raw text.
+		return [["div", `=${r.severity}: ${r.message}`]];
+	})));
+
+	return root;
 }
